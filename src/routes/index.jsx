fbf300423538072a5eafe63833629e7053e8cdbb
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-02 09:37:21 -0300

    correction

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./app.routes";
+// import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
