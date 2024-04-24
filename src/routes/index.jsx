4786a05dfc5430094bb870ff867d639d33350459
Lennon Fonseca <Lennonfonsecaa@gmail.com>
2024-04-24 17:47:25 -0300

    If the user has authenticated, go to app.routes, if not, go to auth.routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
-// import { AppRoutes } from "./app.routes";
+import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
+import { useAuth } from "../hooks/auth";
 
 export function Routes() {
+    const { user } = useAuth()
     return(
         <BrowserRouter>
-        <AuthRoutes />
-        </BrowserRouter>
+        { user ? <AppRoutes /> : <AuthRoutes />}
+        </BrowserRouter> // se user tiver logado ir para app, se não ir para auth.
     )
 }
